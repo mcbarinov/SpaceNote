ae6a7683de897d9a1d0a3690d3e5ad8834119cdd
mcbarinov <mcbarinov@proton.me>
2025-07-05 11:56:02 +0300

    update

--- src/spacenote/web/render.py
+++ src/spacenote/web/render.py
@@ -1,17 +1,48 @@
+from collections.abc import Sequence
+
 from fastapi import Request
 from fastapi.responses import HTMLResponse
 from jinja2 import ChoiceLoader, Environment, PackageLoader
+from markupsafe import Markup
 
 from spacenote.core.field.models import FieldType
 from spacenote.core.user.models import User
+
+
+def empty(value: object) -> object:
+    if value is None:
+        return ""
+    if isinstance(value, Sequence) and len(value) == 0:
+        return ""
+    return value
+
+
+def yes_no(
+    value: object, is_colored: bool = True, hide_no: bool = False, none_is_false: bool = False, on_off: bool = False
+) -> Markup:
+    clr = "black"
+    if none_is_false and value is None:
+        value = False
+
+    if value is True:
+        value = "on" if on_off else "yes"
+        clr = "green"
+    elif value is False:
+        value = "" if hide_no else "off" if on_off else "no"
+        clr = "red"
+    elif value is None:
+        value = ""
+    if not is_colored:
+        clr = "black"
+    return Markup(f"<span style='color: {clr};'>{value}</span>")  # nosec  # noqa: S704
 
 
 def init_jinja() -> Environment:
     loader = ChoiceLoader([PackageLoader("spacenote.web")])
     env = Environment(loader=loader, autoescape=True, enable_async=True)
 
-    env.filters |= {}  # Custom filters can be added here
-    env.globals |= {"field_types": list(FieldType)}  # Custom globals can be added here
+    env.filters |= {"empty": empty, "yes_no": yes_no}
+    env.globals |= {"field_types": list(FieldType)}
     return env
 
 
